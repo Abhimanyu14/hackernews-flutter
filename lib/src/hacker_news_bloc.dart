73ebacb05aba9276088a386fe4fbe81b5db56415
Abhimanyu <abhimanyu.n14@gmail.com>
2021-02-08 22:14:50 +0530

    8.2-code_cleanup-pub_versioning_fix

--- lib/src/hacker_news_bloc.dart
+++ lib/src/hacker_news_bloc.dart
@@ -11,25 +11,14 @@ enum StoriesType {
   newStories,
 }
 
+class HackerNewsApiError extends Error {
+  final String message;
+
+  HackerNewsApiError(this.message);
+}
+
 class HackerNewsBloc {
-  static List<int> _topStoriesIds = [
-    26006656,
-    26010237,
-    26010927,
-    26011025,
-    26007100,
-    26010782,
-  ];
-
-  static List<int> _newStoriesIds = [
-    26009542,
-    25997906,
-    26011309,
-    25993624,
-    25989115,
-    26010977,
-  ];
-
+  static const _baseUrl = 'https://hacker-news.firebaseio.com/v0/';
   final _isLoadingSubject = BehaviorSubject<bool>.seeded(false);
   final _articlesSubject = BehaviorSubject<UnmodifiableListView<Article>>();
   final _storiesTypeController = StreamController<StoriesType>();
@@ -42,14 +31,14 @@ class HackerNewsBloc {
   var _articles = <Article>[];
 
   HackerNewsBloc() {
-    _getArticlesAndUpdate(_topStoriesIds);
-    _storiesTypeController.stream.listen((storiesType) {
-      if (storiesType == StoriesType.newStories) {
-        _getArticlesAndUpdate(_newStoriesIds);
-      } else if (storiesType == StoriesType.topStories) {
-        _getArticlesAndUpdate(_topStoriesIds);
-      }
+    _initializeArticles();
+    _storiesTypeController.stream.listen((storiesType) async {
+      _getArticlesAndUpdate(await _getIds(storiesType));
     });
+  }
+
+  Future<void> _initializeArticles() async {
+    _getArticlesAndUpdate(await _getIds(StoriesType.topStories));
   }
 
   void _getArticlesAndUpdate(List<int> articleIds) async {
@@ -59,19 +48,33 @@ class HackerNewsBloc {
     _isLoadingSubject.add(false);
   }
 
-  Future<Article> _getArticle(int id) async {
-    final itemUrl = 'https://hacker-news.firebaseio.com/v0/item/$id.json';
-    final itemResponse = await http.get(itemUrl);
-    if (itemResponse.statusCode == 200) {
-      return parseArticle(itemResponse.body);
-    } else {
-      return null;
+  Future<List<int>> _getIds(StoriesType storiesType) async {
+    final url = storiesType == StoriesType.topStories
+        ? '${_baseUrl}topstories.json'
+        : '${_baseUrl}newstories.json';
+    final httpResponse = await http.get(url);
+    if (httpResponse.statusCode == 200) {
+      return parseTopStories(httpResponse.body).take(10).toList();
     }
+    throw HackerNewsApiError('Stories $storiesType could not be fetched.');
+  }
+
+  Future<Article> _getArticle(int id) async {
+    final url = '$_baseUrl/item/$id.json';
+    final httpResponse = await http.get(url);
+    if (httpResponse.statusCode == 200) {
+      return parseArticle(httpResponse.body);
+    }
+    throw HackerNewsApiError('Article $id could not be fetched.');
   }
 
   Future<Null> _updateArticles(List<int> articleIds) async {
     final futureArticles = articleIds.map((id) => _getArticle(id));
     final articles = await Future.wait(futureArticles);
     _articles = articles;
+  }
+
+  void close() {
+    _storiesTypeController.close();
   }
 }
